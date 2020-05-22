a4427a8c81835658f0fa67ca0c2820958aa1e22e
smestern <smestern@uwo.ca>
2020-05-22 13:05:24 -0400

    Makes the package installable

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # clustering-stability
 Clustering stability based on https://arxiv.org/pdf/physics/0005046
+
+Script can be installed and imported as cstab
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name='clustering-stability',
+    version='0.1.3',
+    author='Mihin Sumaria',
+    author_email='n/a',
+    packages=['cstab'],
+    url='Mihin Sumaria',
+    license='MIT License',
+    platforms='https://github.com/mihinsumaria',
+    description='',
+    long_description='',
+    install_requires=[	
+       'matplotlib>=2.1.0',
+       'numpy>=1.17'
+	]
+)
